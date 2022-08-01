abe31d3707bd73ccd44e71163f5524de3e475238
Call_Ma_Name <93004055+showWhatUGot@users.noreply.github.com>
2022-08-01 17:05:51 +0900

    clean up 함수

--- hooks-tutorial/src/App.js
+++ hooks-tutorial/src/App.js
@@ -1,7 +1,22 @@
 import Info from "./Info";
+import { useState } from "react";
 
 const App = () => {
-  return <Info />;
+  const [visible, setVisible] = useState(false);
+
+  return (
+    <div>
+      <button
+        onClick={() => {
+          setVisible(!visible);
+        }}
+      >
+        {visible ? "숨기기" : "보이기"}
+      </button>
+      <hr />
+      {visible && <Info />}
+    </div>
+  );
 };
 
 export default App;
